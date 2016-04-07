64880c0911c1185bc301dc90544bbe97b6e0bd10
James Hogarth <james.hogarth@gmail.com>
2016-04-07 19:46:46 +0100

    add ansible dynamic inventory script used in article

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# libvirt_scripts
+# Scripting libvirt
+
+A varied collection of scripts used for quick VM testing
+
+## Ansible libvirt inventory
+
+ansible/libvirt_inv.py is a dynamic ansible inventory script to probe for local VMs
+automatically generate an inventory with IP addresses to avoid looking them up for 
+a flat inventory file, the groups are taken from the desc field of the VM
+
+See [Ansible and libvirt, a marriage made in python](https://www.hogarthuk.com/?q=node/12) for more details.
+
+
---
+++ ansible/libvirt_inv.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+
+'''
+Dynamic inventory of libvirt script for Ansible, in Python.
+'''
+
+from __future__ import unicode_literals
+
+class LibvirtInventory(object): #pylint:disable=missing-docstring
+
+    def __init__(self): #pylint:disable=missing-docstring
+        import sys
+        import libvirt
+        import json
+        self.inventory = {'_meta': {'hostvars': {}}}
+        self.mgmt_net = "192.168.124.0/24"
+        self.read_cli_args()
+        self.conn = libvirt.open()
+        if self.conn == None:
+            print 'Failed to connect to hypervisor'
+            sys.exit(1)
+
+        if self.args.list:
+            self.get_inv()
+        elif self.args.host:
+            #self.dom_info(self.args.host)
+            pass
+
+        print json.dumps(self.inventory)
+
+    def get_inv(self): #pylint:disable=missing-docstring
+        domains = self.conn.listDomainsID()
+        if len(domains) == 0:
+            return
+        else:
+            for domain in domains:
+                self.dom_info(self.get_domain(domain))
+
+    def get_domain(self, dom): #pylint:disable=missing-docstring
+        if isinstance(dom, int):
+            domain = self.conn.lookupByID(dom)
+        else:
+            domain = self.conn.lookupByName(dom)
+        return domain
+
+    @staticmethod
+    def get_domain_desc(domain): #pylint:disable=missing-docstring
+        import libvirt
+        import json
+        try:
+            dom_inv = json.loads(domain.metadata(0, None))
+        except (ValueError, libvirt.libvirtError):
+            dom_inv = {}
+
+        return dom_inv
+
+    @staticmethod
+    def is_mgmt_net(addr, mgmt_net): #pylint: disable=missing-docstring
+        import ipaddress
+
+        if addr['type'] == 0 and \
+          ipaddress.ip_address(unicode(addr['addr'])) in ipaddress.ip_network(mgmt_net):
+            return True
+        else:
+            return False
+
+
+
+    def dom_info(self, domain): #pylint:disable=missing-docstring
+        import libvirt
+
+        dom_inv = self.get_domain_desc(domain)
+
+        try:
+            dom_host_vars = {}
+            dom_ifaces = domain.interfaceAddresses(libvirt.VIR_DOMAIN_INTERFACE_ADDRESSES_SRC_AGENT)
+            if dom_ifaces != None:
+                for iface in dom_ifaces:
+                    for addr in dom_ifaces[iface]['addrs']:
+                        if self.is_mgmt_net(addr, self.mgmt_net):
+                            dom_host_vars['ansible_host'] = addr['addr']
+            if 'ansible_host' not in dom_host_vars:
+                return
+            if 'groups' in dom_inv:
+                for group in dom_inv['groups']:
+                    if group in self.inventory:
+                        self.inventory[group]['hosts'].append(domain.name())
+                    else:
+                        self.inventory.update({group: {'hosts': [domain.name()]}})
+            dom_host_vars['ansible_user'] = 'root'
+            if 'hostvars' in dom_inv:
+                dom_host_vars.update(dom_inv['hostvars'])
+            self.inventory['_meta']['hostvars'].update({domain.name(): dom_host_vars})
+        except (TypeError, libvirt.libvirtError):
+            pass
+
+    def read_cli_args(self): #pylint:disable=missing-docstring
+        import argparse
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--list', action='store_true')
+        parser.add_argument('--host', action='store')
+        self.args = parser.parse_args()
+
+
+LibvirtInventory()
